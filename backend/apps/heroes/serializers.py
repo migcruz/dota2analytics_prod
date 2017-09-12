9e63778cb1b348e3bae93007bdad297dbb95f825
migcruz <miguel.ramon.cruz@gmail.com>
2017-09-12 15:49:45 -0700

    updated hero model

--- backend/apps/heroes/serializers.py
+++ backend/apps/heroes/serializers.py
@@ -4,4 +4,7 @@ from .models import Hero
 class HeroSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hero
-        fields = ('id', 'name')
+        fields = ('id', 'hero_id', 'npc_name', 'name', 'npc_name', 'primary_attr', 'attack_type', 'roles', 'webm',
+                  'icon', 'base_health', 'base_health_regen', 'base_mana', 'base_mana_regen', 'base_armor', 'base_mr', 'base_attack_min',
+                  'base_attack_max', 'base_str', 'base_agi', 'base_int', 'str_gain', 'agi_gain', 'int_gain', 'attack_range',
+                  'projectile_speed', 'attack_rate', 'move_speed', 'turn_rate', 'cm_enabled', 'legs' )
