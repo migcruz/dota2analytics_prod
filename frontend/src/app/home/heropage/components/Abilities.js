e7636dcaa8bac6bd6cc3f65102f81108c258c7fb
migcruz <miguel.ramon.cruz@gmail.com>
2017-09-28 16:48:02 -0700

    updates

--- frontend/src/app/home/heropage/components/Abilities.js
+++ frontend/src/app/home/heropage/components/Abilities.js
@@ -42,6 +42,13 @@ class Abilities extends React.Component {
             else {
                 row.push(
                     <Grid.Column key={this.abilities[i]["ability_name"]}>
+                        <div style={{position: 'relative', top: '0', left: '0'}}>
+                            <Image.Group>
+                                <Image src={`${staticRoot}dota2assets/img/items_25x18/black_king_bar.png`}/>
+                                <Image src={`${staticRoot}dota2assets/img/items_25x18/black_king_bar.png`}/>
+                                <Image src={`${staticRoot}dota2assets/img/items_25x18/black_king_bar.png`}/>
+                            </Image.Group>
+                        </div>
                         <div className="HeroGrid-wrapper">
                             <div className="HeroGrid-name">
                                 <h5>{this.abilities[i]["ability_info"]["dname"]}</h5>
@@ -49,7 +56,7 @@ class Abilities extends React.Component {
                             <div className="HeroGrid-hero">
                                 {/* Put box shadow after image to give effect https://stackoverflow.com/questions/21414925/why-doesnt-inset-box-shadow-work-over-images */}
                                 <Image src={`${staticRoot}${this.abilities[i]["ability_info"]["img"]}`} alt="skill"/>
-                                <div style={{boxShadow: '0 1px 0 rgba(255,255,255,.2), inset 0 4px 5px rgba(0,0,0,.6), inset 0 0.5px 0 rgba(0,0,0,.6)', position: 'absolute', width: '100%', height: '100%', top: '0', left: '0'}}/>
+                                <div style={{boxShadow: '0 1px 0 rgba(255,255,255,.1), inset 0 4px 5px rgba(0,0,0,.9), inset 0 0.5px 0 rgba(0,0,0,.9)', position: 'absolute', width: '100%', height: '100%', top: '0', left: '0'}}/>
                             </div>
                         </div>
                     </Grid.Column>
