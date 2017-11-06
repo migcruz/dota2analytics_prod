2e779fce048b6d57612ca5a60336a98d06d2c245
migcruz <miguel.ramon.cruz@gmail.com>
2017-11-06 08:44:45 -0800

    added import

--- backend/settings/common.py
+++ backend/settings/common.py
@@ -126,5 +126,6 @@ AUTH_USER_MODEL = 'users.EmailUser'
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' # need this for heroku local and maybe heroku in general
 
 # Update database configuration with $DATABASE_URL.
+import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500) #
 DATABASES['default'].update(db_from_env) #
